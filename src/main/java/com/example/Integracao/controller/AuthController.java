47a715900859c376940f43a90d0a2054b2b2b34a
Matheus Cruz <matheus_silva-cruz@estudante.sesisenai.org.br>
2024-04-22 10:00:48 -0300

    Modifica Controller

--- src/main/java/com/example/Integracao/controller/AuthController.java
+++ src/main/java/com/example/Integracao/controller/AuthController.java
@@ -4,13 +4,19 @@ import com.example.Integracao.model.Usuario;
 import com.example.Integracao.repository.UsuarioRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:5173")//Endereço do front
 @RestController
 @RequestMapping("/api")
 public class AuthController {
@@ -21,7 +27,10 @@ public class AuthController {
  public AuthController(UsuarioRepository usuarioRepository) {
  this.usuarioRepository = usuarioRepository;
  }
-
+    @GetMapping
+    public List<Usuario> listarUsuario() {
+        return usuarioRepository.findAll();
+    }
 @PostMapping("/login")
  public Boolean login(@RequestBody Usuario usuario) {
      List<Usuario> usuarios = usuarioRepository.findAll();
@@ -33,4 +42,26 @@ public class AuthController {
  }
  return false;
  }
+      @DeleteMapping("/{cod}")
+    public ResponseEntity<String> deletarUsuario(@PathVariable Long cod) {
+        if (usuarioRepository.existsById(cod)) {
+            usuarioRepository.deleteById(cod);
+            return ResponseEntity.ok("Usuario deletado com sucesso.");
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+        @PutMapping("/{cod}")
+    public ResponseEntity<Usuario> atualizarUsuario(@PathVariable(name = "cod") Long cod, @RequestBody Usuario usuarioAtualizado) {
+        if (usuarioRepository.existsById(cod)) {
+            Usuario usuario = usuarioRepository.findById(cod).get();
+            usuario.setUsername(usuarioAtualizado.getUsername());
+            usuario.setPassword(usuarioAtualizado.getPassword());
+
+            Usuario usuarioAtualizadoBD = usuarioRepository.save(usuario);
+            return ResponseEntity.ok(usuarioAtualizadoBD);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
